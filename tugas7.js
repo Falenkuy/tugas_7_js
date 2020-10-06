51a05cecaad4211445fb108c926125ee080ea662
Valencinus Aten <valenaten22@gmail.com>
2020-10-06 15:14:28 +0700

    Tugas 7 JS
    Tugas ini saya kumpulkan ke tim Niomic

---
+++ tugas7.js
@@ -0,0 +1,9 @@
+var tinggiBadan = [170,145,120,123,125,156,178,145,172,122]
+
+for( var i = 0 ; i <=tinggiBadan.length ; i++){
+console.log(tinggiBadan[i]);
+}
+
+for(let x of tinggiBadan){
+  console.log(x)
+}
